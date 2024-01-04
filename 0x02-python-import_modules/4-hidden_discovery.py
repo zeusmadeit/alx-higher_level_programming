a61feadc9b7a1282999c25417cda424c38b63b92
Abraham Paul <codexapp44@gmail.com>
2024-01-04 14:49:27 -0800

    task4 done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for name in names:
+    new_names = sorted(names)
+    for name in new_names:
         if name[:2] != "__":
             print(name)
