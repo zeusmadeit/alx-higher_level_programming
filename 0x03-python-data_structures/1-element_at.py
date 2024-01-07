203c9f9f272c3aa6363a24789a43d90d76f112e6
Abraham Paul <codexapp44@gmail.com>
2024-01-07 02:59:35 -0800

    task 1 modified

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
 	count = len(my_list)
-	if idx < 0:
-		print ('None')
-	elif idx > count - 1:
-		print ('None')
+	if idx < 0 or idx >= count:
+		return ('None')
 	else:
 		return my_list[idx]
