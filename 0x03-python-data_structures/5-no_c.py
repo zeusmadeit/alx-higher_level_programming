3db01e46652db49fe4f40f39814bc73f612f6365
Abraham Paul <codexapp44@gmail.com>
2024-01-07 04:14:20 -0800

    task5 done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-	for el in range(0, len(my_string)):
-		if my_string[el] == 'c' or my_string[el] == 'C':
-			my_string[el] = ''
-	return my_string
+	new_str = ''
+	for el in my_string:
+		if el != 'c' and el != 'C':
+			new_str += el
+	return new_str
