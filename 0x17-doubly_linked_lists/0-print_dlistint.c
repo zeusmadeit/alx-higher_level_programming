228cdfede4072867fdd6e18ffd1259246b89b329
Abraham Paul <codexapp44@gmail.com>
2024-01-11 12:58:45 -0800

    task 0 done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,13 +2,13 @@
 
 /**
  * print_dlistint - prints all the elements of a list
- * h: list
+ * @h - list
  * Return: count.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
-	dlistint_t current = h;
+	dlistint_t *current = h;
 
 	count = 0;
 	while (current != NULL)
