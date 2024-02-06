57185f503d86a97d90cfc657e40ac2b3bd3ad212
Abraham Paul <blackapp44@gmail.com>
2024-02-06 18:47:18 +0000

    task 0 complete

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""This module defines a text file reading function"""
 
 def read_file(filename=""):
     """
-    Function that reads a text file (UTF8) and prints it to stdout
+    Function that reads a text file in (UTF8) and prints it to stdout
 
     Agrs:
         filename (string): name of the file to read
     """
-    with open(filename, 'r', encoding="UTF8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
             print(line)
