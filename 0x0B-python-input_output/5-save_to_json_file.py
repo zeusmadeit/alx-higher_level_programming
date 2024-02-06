83b25b5b153fdf4473515bd8a98609e7d937cb5d
Abraham Paul <blackapp44@gmail.com>
2024-02-06 19:45:29 +0000

    task 5 complete

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,4 +10,5 @@ def save_to_json_file(my_obj, filename):
     my_obj (object): a python object of any type
     filename (file): file to write to
     """
-    return json.dump(my_obj, filename)
+    with open(filename, 'w', encoding='utf-8') as jf:
+    json.dump(my_obj, jf)
