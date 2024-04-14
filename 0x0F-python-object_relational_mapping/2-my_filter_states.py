c7870eca3ddfdaa346662e4b84784a60f62c2eb9
zeusmadeit <58306978+zeusmadeit@users.noreply.github.com>
2024-04-14 14:06:39 +0000

    modified task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """
-This script that takes in an argument and displays all values in 
-the states table of hbtn_0e_0_usa where name matches the argument
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
-import sys
-import MySQLdb
 
-if __name__ == "__main__":
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
     """
-    Access the database and get the states
+    Access to the database and get the states
     from the database.
     """
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
-                         passwd=sys.argv[2], db=sys.argv[3], 
-                         port=3306)
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY {} \
-            ORDER BY states.id ASC".format(sys.argv[4]))
-    rows = cur.fetchall()
+    db = MySQLdb.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+                        states.id ASC".format(argv[4]))
+    rows = cursor.fetchall()
+
     for row in rows:
         print(row)
