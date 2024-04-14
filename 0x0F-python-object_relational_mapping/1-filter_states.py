390140761234bc947bf9f984f1043f28b9e4e092
zeusmadeit <58306978+zeusmadeit@users.noreply.github.com>
2024-04-14 13:43:18 +0000

    task 1 done

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting 
+with N (upper N) from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE REGEXP_LIKE(name, '^N') ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        for col in row:
+            print("%s,".format(col))
+        print ("\n")
