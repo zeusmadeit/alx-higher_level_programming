19237e6ba31a59164fb37e37c1826256276d3853
zeusmadeit <58306978+zeusmadeit@users.noreply.github.com>
2024-04-14 15:03:23 +0000

    task 5 done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
-This script lists all cities from the database hbtn_0e_4_usa
+This script takes in the name 
+of a state as an argument and 
+lists all cities of that state, 
+using the database hbtn_0e_4_usa
 """
 
 import MySQLdb
@@ -15,8 +18,8 @@ if __name__ == '__main__':
                             user=argv[1], passwd=argv[2], db=argv[3])
 
     with db.cursor() as cursor:
-        cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states \
-                       ON cities.state_id = states.id ORDER BY cities.id ASC")
+        cursor.execute("SELECT cities.name FROM cities WHERE states.name = %(state)s JOIN states \
+                       ON cities.state_id = states.id ORDER BY cities.id ASC", {"state", argv[4]})
         rows = cursor.fetchall()
 
     if rows is not None:
