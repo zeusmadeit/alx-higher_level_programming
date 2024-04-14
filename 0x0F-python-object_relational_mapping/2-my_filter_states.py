f2fc3b188c55d95645473b8a00a5d88229a7fb1e
zeusmadeit <58306978+zeusmadeit@users.noreply.github.com>
2024-04-14 14:02:54 +0000

    task 2 done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                          port=3306)
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name=%s \
+        "SELECT * FROM states WHERE name LIKE BINARY {} \
             ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
