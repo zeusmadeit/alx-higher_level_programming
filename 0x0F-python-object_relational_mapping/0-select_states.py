f9b426804441fdcdaf4b5f953062d79510904402
zeusmadeit <58306978+zeusmadeit@users.noreply.github.com>
2024-04-14 13:51:00 +0000

    made minor change to task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""a script that lists all states from the database hbtn_0e_0_usa"""
+"""
+a script that lists all states 
+from the database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
-        for col in row:
-            print("%s,".format(col))
-        print ("\n")
+        print(row)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,12 +4,19 @@ with N (upper N) from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
+"""
+Access to the database and get the states
+from the database.
+"""
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
+                         passwd=sys.argv[2], db=sys.argv[3], 
+                         port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE REGEXP_LIKE(name, '^N') ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE REGEXP_LIKE(name, '^N') \
+            ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
-        for col in row:
-            print("%s,".format(col))
-        print ("\n")
+        print(row)
