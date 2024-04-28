4bdd4abf0d12e986f7c666a133d8d913fb9c50c2
zeusmadeit <58306978+zeusmadeit@users.noreply.github.com>
2024-04-28 11:38:31 +0000

    modified task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request to a URL with curl, and displays the size of the body of the response
+# send a request to a URL with curl, and display the size of the body response
 curl -s "$1" | wc -c
